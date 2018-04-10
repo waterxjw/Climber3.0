e1311210dfc65d0be1ad7e43f2ade12ad707a94c
waterxjw <171250675@smail.nju.edu.cn>
2018-04-10 09:46:23 +0800

    把Ending合并进去，完成合并
    把Ending合并进去，完成合并

---
+++ app/app/src/main/java/com/example/xjw/climber/Ending.java
@@ -0,0 +1,24 @@
+package com.example.xjw.climber;
+import android.app.Activity;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import com.example.xjw.climber.Border;
+
+
+public class Ending extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_ending);
+    }
+}
