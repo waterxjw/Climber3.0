d743b395b540156327f0fcd866d5ecf0ba8cea4e
DW <171250507@smail.nju.edu.cn>
2018-04-13 00:23:07 +0800

    6.0  动图    下一阶段：缓存。 晚~

--- app/app/src/main/java/com/example/xjw/climber/Climbing.java
+++ app/app/src/main/java/com/example/xjw/climber/Climbing.java
@@ -2,6 +2,7 @@ package com.example.xjw.climber;
 
 import android.app.Service;
 import android.content.Intent;
+import android.graphics.drawable.AnimationDrawable;
 import android.os.Build;
 import android.os.Bundle;
 import android.os.CountDownTimer;
@@ -14,10 +15,14 @@ import android.os.Vibrator;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.widget.Chronometer;
+import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
 
 public class Climbing extends AppCompatActivity {
+
+    private AnimationDrawable animationDrawable;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -30,9 +35,9 @@ public class Climbing extends AppCompatActivity {
         //剩余时间输出
         final TextView tv = findViewById(R.id.timeOut);
 
-        int[] var = TimeGet.getInstance().getTime();
-        final int hours = var[0];
-        final int minute = var[1];
+        int[] varTime = TimeGet.getInstance().getTime();
+        final int hours = varTime[0];
+        final int minute = varTime[1];
 
 
         //已用时间输出
@@ -54,7 +59,7 @@ public class Climbing extends AppCompatActivity {
                 tv.setText("到达");
                 TextView tv2 = findViewById(R.id.outPrompt);
                 tv2.setText("");
-                Intent intent2=new Intent(Climbing.this,Ending.class);
+                Intent intent2 = new Intent(Climbing.this, Ending.class);
                 startActivity(intent2);
             }
         }.start();
@@ -110,6 +115,31 @@ public class Climbing extends AppCompatActivity {
             }
         }).start();
 
+
+        //图片问题
+        ImageView img = findViewById(R.id.imageView2);
+        img.setImageResource(R.drawable.animation);
+        animationDrawable = (AnimationDrawable) img.getDrawable();
+
     }
+
+
+
+    //emmmmmmmmm
+    @Override
+    protected void onResume() {
+        super.onResume();
+        animationDrawable.start();
+    }
+
+    //释放内存，但是应该在锁屏后执行
+    @Override
+    protected void onPause() {
+        super.onPause();
+        if (animationDrawable != null) {
+            animationDrawable.stop();
+        }
+    }
+
 
 }
