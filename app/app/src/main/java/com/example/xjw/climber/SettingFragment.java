bfc9cd83b5126f2fb84becbc651b6acf541a1ea2
waterxjw <171250675@smail.nju.edu.cn>
2018-04-22 12:10:55 +0800

    加入设置界面，加了个彩蛋娱乐一下
    加入设置界面，加了个彩蛋娱乐一下

---
+++ app/app/src/main/java/com/example/xjw/climber/SettingFragment.java
@@ -0,0 +1,22 @@
+package com.example.xjw.climber;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.preference.Preference;
+import android.support.v7.preference.PreferenceFragmentCompat;
+import android.support.v7.preference.PreferenceScreen;
+
+/**
+ * Created by xjw on 2018/4/22.
+ */
+
+public class SettingFragment extends PreferenceFragmentCompat {
+    @Override
+    public void onCreatePreferences(Bundle bundle, String s) {
+        // Load the Preferences from the XML file
+        addPreferencesFromResource(R.xml.prefs);
+
+    }
+    
+
+}
