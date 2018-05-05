b465f74f64222d0e4688b7b9225c5188c4567222
SimpleLin <171250015@smail.nju.edu.cn>
2018-05-05 20:27:03 +0800

    清空master

--- app/app/src/main/java/com/example/xjw/climber/Features.java
+++
@@ -1,7 +0,0 @@
-package com.example.xjw.climber;
-
-public class Features {
-    public static int BGK_METHOD = 3;
-    public static boolean showForeground = false;
-    public static boolean showProfile = true;
-}
