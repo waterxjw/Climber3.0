3141e79e1d436000c315863020da7c0607a8a4fd
DW <171250507@smail.nju.edu.cn>
2018-05-21 23:21:10 +0800

    no message

--- app/app/src/main/java/com/hlxx/climber/secondpage/records/Record.java
+++ app/app/src/main/java/com/hlxx/climber/secondpage/records/Record.java
@@ -54,8 +54,9 @@ public class Record implements Serializable {
                 ", finish=" + finish +
                 ", switchTimes=" + switchTimes +
                 ", totalTime=" + totalTime +
-                ", now=" + now +
-                ", level=" + level +
+                ", now=[" + now.get(Calendar.MONTH)+":"+now.get(Calendar.DAY_OF_MONTH+1) +"   "
+                +now.get(Calendar.HOUR)+":"+now.get(Calendar.MINUTE)+":"+now.get(Calendar.SECOND)+
+                "], level=" + level +
                 '}';
     }
 
