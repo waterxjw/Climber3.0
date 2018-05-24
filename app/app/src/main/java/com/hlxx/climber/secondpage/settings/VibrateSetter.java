dbe9b2f603d6a539227581ad59a2c32a2b7a9e9d
DW <171250507@smail.nju.edu.cn>
2018-05-25 01:28:26 +0800

    震动，快吐血了

--- app/app/src/main/java/com/hlxx/climber/secondpage/settings/VibrateSetter.java
+++ app/app/src/main/java/com/hlxx/climber/secondpage/settings/VibrateSetter.java
@@ -6,58 +6,66 @@ import android.app.NotificationManager;
 import android.graphics.BitmapFactory;
 import android.graphics.Color;
 import android.os.Build;
+import android.support.v4.app.NotificationCompat;
 import com.hlxx.climber.R;
 import com.hlxx.climber.secondpage.ClimbingActivity;
 
 import java.lang.ref.WeakReference;
 
 import static android.content.Context.NOTIFICATION_SERVICE;
+import static com.hlxx.climber.firstpage.setting.SharedPreferenceUtils.getBoolean;
 
 public class VibrateSetter {
     private WeakReference<ClimbingActivity> theActivity;
-    private static boolean isVibrate = false;
+    private static boolean isVibrate;
 
-    public static void setVibrate(boolean vibrate) {
-        isVibrate = vibrate;
-    }
 
     public VibrateSetter(ClimbingActivity theActivity) {
-        this.theActivity = new WeakReference<ClimbingActivity>(theActivity);
+        this.theActivity = new WeakReference<>(theActivity);
     }
 
     public void makeVibrate(boolean finish) {
 
         ClimbingActivity activity = theActivity.get();
         //处理震动
-        NotificationManager manager =
-                (NotificationManager) activity.getSystemService(NOTIFICATION_SERVICE);
+        isVibrate = getBoolean(activity, "remind");
+
+        NotificationManager manager = (NotificationManager) activity.getSystemService(NOTIFICATION_SERVICE);
         NotificationChannel mNotificationChannel;
+        NotificationChannel mNotificationChannel_NoVibrate;
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
-            mNotificationChannel = new NotificationChannel("default", "verible", NotificationManager.IMPORTANCE_DEFAULT);
+            mNotificationChannel = new NotificationChannel("Vibrate", "Vibrate", NotificationManager.IMPORTANCE_DEFAULT);
             mNotificationChannel.setDescription("Exit");
             mNotificationChannel.enableLights(true);
-            if (isVibrate) {
-                mNotificationChannel.enableVibration(true);
-                mNotificationChannel.setVibrationPattern(new long[]{0, 1500});
-            }else {
-                mNotificationChannel.enableVibration(false);
-            }
+            mNotificationChannel.enableVibration(true);
+            mNotificationChannel.setVibrationPattern(new long[]{0, 1500, 500, 1000});
             manager.createNotificationChannel(mNotificationChannel);
+
+            mNotificationChannel_NoVibrate = new NotificationChannel("NoVibrate", "NoVibrate", NotificationManager.
+                    IMPORTANCE_LOW);
+            mNotificationChannel_NoVibrate.setDescription("Exit");
+            mNotificationChannel_NoVibrate.enableLights(true);
+            mNotificationChannel_NoVibrate.enableVibration(false);
+            mNotificationChannel.setVibrationPattern(new long[]{} );
+            manager.createNotificationChannel(mNotificationChannel_NoVibrate);
         }
+
         Notification.Builder builder;
         if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.O) {
-            builder = new Notification.Builder(activity, "default");
-        } else {
-            builder = new Notification.Builder(activity)
-                    .setLights(Color.WHITE, 3000, 500);
             if (isVibrate) {
-                builder.setVibrate(new long[]{0, 1500});
+                builder = new Notification.Builder(activity, "Vibrate");
+            } else {
+                builder = new Notification.Builder(activity, "NoVibrate");
+            }
+        } else {
+            builder = new Notification.Builder(activity).setLights(Color.WHITE, 3000, 500);
+            if (isVibrate) {
+                builder.setVibrate(new long[]{0, 1500, 500, 1000});
             }
         }
-        //设置 通知 图标
         builder.setSmallIcon(R.mipmap.ic_launcher);
-        builder.setLargeIcon(BitmapFactory.decodeResource(activity.getResources(), R.mipmap.ic_launcher_round));
         builder.setAutoCancel(true);
+        builder.setLargeIcon(BitmapFactory.decodeResource(activity.getResources(), R.mipmap.ic_launcher_round));
         if (finish) {
             builder.setTicker("恭喜");
             builder.setContentTitle("成功");
@@ -67,10 +75,8 @@ public class VibrateSetter {
             builder.setContentTitle("失败");
             builder.setContentText("切换过多，下次加油！");
         }
-        Notification notification = null;
-        notification = builder.build();
-        notification.flags = Notification.FLAG_SHOW_LIGHTS;
-        notification.flags = Notification.FLAG_AUTO_CANCEL;
+        Notification notification = builder.build();
+        notification.flags |= Notification.FLAG_AUTO_CANCEL;
         manager.notify(5, notification);
     }
 }
