cdf0e86741cdc5c9c9b92bf3f7ac9e3187efc821
waterxjw <171250675@smail.nju.edu.cn>
2018-05-24 17:29:21 +0800

    删了一些无用代码，部分调整美化

--- app/app/src/main/java/com/hlxx/climber/firstpage/TimeSettingActivity.java
+++ app/app/src/main/java/com/hlxx/climber/firstpage/TimeSettingActivity.java
@@ -5,8 +5,6 @@ import android.content.Intent;
 import android.graphics.Color;
 import android.os.Bundle;
 import android.support.v4.app.ActivityCompat;
-import android.support.v4.app.ActivityOptionsCompat;
-import android.support.v4.util.Pair;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
